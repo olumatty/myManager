c9f48cda07066f6348e123b7b1e8624064c198d0
olumatty <olumatty@gmail.com>
2025-07-23 22:15:33 +0100

    chore: backend packages installed

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -7,7 +7,13 @@ export const registerSchema = z.object({
       .min(3, "First name must be at least 1 character long"),
     lastName: z.string().min(3, "Last name must be at least 1 character long"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters long"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+        "Password must contain uppercase, lowercase, number and special character",
+      ),
   }),
 });
 export const loginSchema = z.object({
